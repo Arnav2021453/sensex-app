f3fb318818100c2835433b6fbe1506bab26d0826
Arnav2021453 <arnav21453@iiitd.ac.in>
2025-07-08 19:46:42 +0530

    Auto Logout on Token Expire

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import {jwtDecode} from 'jwt-decode';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apiUrl='http://localhost:3000';
   private token: string | null = null;
-
-  constructor(private http: HttpClient, private router: Router) { }
+  private logouttimer:any;
+  constructor(private http: HttpClient, private router: Router) {
+    this.autoLogoutAfterTokenExpires();
+   }
   login(email: string, password: string) {
     return this.http.post<{ token: string }>(`${this.apiUrl}/auth/login`,{email,password});
   }
   saveToken(token: string) {
     this.token = token;
     localStorage.setItem('token', token);
+    this.autoLogoutAfterTokenExpires();
   }
   getToken(): string | null {
     return this.token || localStorage.getItem('token');
@@ -22,9 +26,44 @@ export class AuthService {
   logout(){
     this.token = null;
     localStorage.removeItem('token');
+    clearTimeout(this.logouttimer);
     this.router.navigate(['/login']);
   }
+  // isLoggedIn(): boolean {
+  //   return !!this.getToken();
+  // }
   isLoggedIn(): boolean {
-    return !!this.getToken();
+    const token=this.getToken();
+    if(!token) return false;
+    try{
+      const decoded:any=jwtDecode(token);
+      const exp=decoded.exp;
+      const now=Math.floor(Date.now()/1000);
+      return exp>now;
+    }
+    catch(e){
+      return false;
+    }
+  }
+  private autoLogoutAfterTokenExpires() {
+    const token=this.getToken();
+    if(!token) return;
+    try{
+      const decoded:any=jwtDecode(token);
+      const expiresAt=decoded.exp*1000;
+      const delay=expiresAt-Date.now();
+      if(delay>0){
+        this.logouttimer=setTimeout(()=>{
+          alert('Session expired. Please login again.');
+          this.logout();
+        },delay);
+      }
+      else{
+        this.logout();
+      }
+    }
+    catch(e){
+      this.logout();
+    }
   }
 }
